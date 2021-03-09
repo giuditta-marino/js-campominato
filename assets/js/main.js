714a1afeee0b8391077b06274c4706e1f88c2caa
Giuditta <giudittamarino89@gmail.com>
2021-03-09 17:34:31 +0100

    bonus

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,38 @@
+// CHIEDO ALL'UTENTE LIVELLO DI DIFFICOLTA'
+var livello = parseInt(prompt("Scegli un livello di difficoltà tra 0 e 2:"));
+
+
 // CREO UN ARRAY PER INSERIRCI DENTRO I NUMERI BOMBA
 bombsArray = [];
 
-// APPLICO LA FUNZIONE INSERENDO NEI PARAMETRI IL MIO ARRAY E IL NUMERO MAX DEL RANGE
-bombs = numberRandomInRange(bombsArray, 100);
-console.log(bombsArray);
+
+// CREO UN ARRAY PER INSERIRCI I NUMERI FORNITI DALL'UTENTE
+safeArray = [];
+
+// APPLICO LA FUNZIONE CAMBIANDO IL PARAMETRO MAX IN BASE AL LIVELLO SCELTO DALL'UTENTE
+switch (livello) {
+  case "0":
+  bombs = numberRandomInRange(bombsArray, 100);
+  console.log(bombsArray);
+  var punteggio = game(bombsArray, safeArray, 100, "Inserisci un numero compreso tra 1 e 100 e prova ad evitare le bombe:");
+
+    break;
+  case "1":
+  bombs = numberRandomInRange(bombsArray, 80);
+  console.log(bombsArray);
+  var punteggio = game(bombsArray, safeArray, 80, "Inserisci un numero compreso tra 1 e 80 e prova ad evitare le bombe:");
+
+    break;
+
+  case "2":
+  bombs = numberRandomInRange(bombsArray, 50);
+  console.log(bombsArray);
+  var punteggio = game(bombsArray, safeArray, 50, "Inserisci un numero compreso tra 1 e 50 e prova ad evitare le bombe:");
+
+    break;
+  default:
+    alert('Scrivi un numero tra 0 e 2');
+}
 
 // CREO UNA FUNZIONE PER GENERARE UN NUMERO RANDOM ENTRO UN RANGE CHE INSERISCA IL NUMERO DENTRO UN ARRAY SE IL NUMERO NON E' GIA PRESENTE ALL'INTERNO DI ESSO (PER EVITARE I NUMERI DUPLICATI)
 function numberRandomInRange(array, max){
@@ -17,9 +46,6 @@ function numberRandomInRange(array, max){
 }
 
 
-// CREO UN ARRAY PER INSERIRCI I NUMERI FORNITI DALL'UTENTE
-safeArray = [];
-
 // RICHIAMO LA FUNZIONE
 var punteggio = game(bombsArray, safeArray, 100);
 document.getElementById('punteggio').innerHTML = "Game over" + "<br>" + "Il tuo punteggio è: " + punteggio;
@@ -30,10 +56,9 @@ console.log(punteggio);
   // CONTROLLO CHE IL NUMERO NON SIA RIPETUTO
     // CONTROLLO CHE IL NUMERO SIA UN NUMERO
       // CONTROLLO SE IL NUMERO E' PRESENTE NELL'ARRAY, SE LO E' FACCIO TERMINARE LA PARTITA, ALTRIMENTI CHIEDO UN ALTRO NUMERO
-
-function game(bombsArray, safeArray, max){
+function game(bombsArray, safeArray, max, promptText){
   while (safeArray.length < max - 16) {
-    var numeroUtente = parseInt(prompt("Inserisci un numero compreso tra 1 e 100 e prova ad evitare le bombe:"));
+    var numeroUtente = parseInt(prompt(promptText));
 
     if (!isNaN(numeroUtente) && 1 <= numeroUtente && numeroUtente <= max && !safeArray.includes(numeroUtente)) {
       if (!bombsArray.includes(numeroUtente)) {
@@ -48,3 +73,6 @@ function game(bombsArray, safeArray, max){
   }
   return safeArray.length;
 }
+
+// CHIEDO ALL'UTENTE LIVELLO DI DIFFICOLTA'
+// SE IL LIVELLO DI DIFFICOLTA' E' 0 IL RANGE DI NUMETRI E' TRA 1 E 100, CON LIVELLO 1 TRA 1 E 80, CON LIVELLO 2 TRA 1 E 50;
